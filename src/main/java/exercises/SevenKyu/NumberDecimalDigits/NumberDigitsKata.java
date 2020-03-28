109239904b865ba52e042251fdce149b39dbb620
Michal Kulpa <m.kulpa@wp.eu>
2020-03-28 16:39:04 +0100

    NumberDecimalDigits 7kyu

---
+++ src/main/java/exercises/SevenKyu/NumberDecimalDigits/NumberDigitsKata.java
@@ -0,0 +1,13 @@
+package exercises.SevenKyu.NumberDecimalDigits;
+
+public class NumberDigitsKata {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int Digits(long n) {
+
+        return String.valueOf(n).length();
+    }
+}

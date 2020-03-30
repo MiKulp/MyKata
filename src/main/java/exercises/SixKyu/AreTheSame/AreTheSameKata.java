4a78af2ba97338141c941182ce432fadfdee1cea
Michal Kulpa <m.kulpa@wp.eu>
2020-03-30 23:00:31 +0200

    AreTheSameKata 6kyu

---
+++ src/main/java/exercises/SixKyu/AreTheSame/AreTheSameKata.java
@@ -0,0 +1,22 @@
+package exercises.SixKyu.AreTheSame;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AreTheSameKata {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean comp(int[] a, int[] b) {
+        if (a == null || b == null) return false;
+
+        List<Integer> listA = Arrays.stream(a).map(i -> (i * i)).boxed().sorted(Integer::compareTo).collect(Collectors.toList());
+
+        List<Integer> listB = Arrays.stream(b).boxed().sorted(Integer::compareTo).collect(Collectors.toList());
+
+        return listA.toString().equals(listB.toString());
+    }
+}

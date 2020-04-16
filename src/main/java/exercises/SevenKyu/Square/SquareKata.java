5578c2201f46edf41ce1ded73d9355f6d95418f8
Michal Kulpa <m.kulpa@wp.eu>
2020-04-16 18:16:59 +0200

    Square 7kyu

---
+++ src/main/java/exercises/SevenKyu/Square/SquareKata.java
@@ -0,0 +1,12 @@
+package exercises.SevenKyu.Square;
+
+public class SquareKata {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isSquare(int n) {
+
+        return Math.sqrt(n) % 1 == 0 ? true : false;
+    }
+}

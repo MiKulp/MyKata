28b940e0a4044163575894ab13f2e8cd4ec51f94
Michal Kulpa <m.kulpa@wp.eu>
2020-05-14 23:27:39 +0200

    SetAlarm 8kyu

---
+++ src/main/java/exercises/EightKyu/SetAlarm/SetAlarmKata.java
@@ -0,0 +1,11 @@
+package exercises.EightKyu.SetAlarm;
+
+public class SetAlarmKata {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean setAlarm(boolean employed, boolean vacation) {
+        return employed==true && vacation==false ? true : false;
+    }
+}

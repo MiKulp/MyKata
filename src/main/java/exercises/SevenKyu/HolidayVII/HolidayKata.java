6438cedf33700bb8509d23783b4ad17fb9d57b4f
Michal Kulpa <m.kulpa@wp.eu>
2020-07-06 12:35:08 +0200

    HolidayKata 7kyu

---
+++ src/main/java/exercises/SevenKyu/HolidayVII/HolidayKata.java
@@ -0,0 +1,14 @@
+package exercises.SevenKyu.HolidayVII;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class HolidayKata {
+
+
+    public static String pak(final String s) {
+      return String.join(" pak ", s.split(" "));
+
+    }
+
+}

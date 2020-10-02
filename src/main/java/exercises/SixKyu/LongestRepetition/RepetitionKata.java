5cf32cb328b43612c8b2ddd958b192a8f38be54c
Michal Kulpa <m.kulpa@wp.eu>
2020-10-02 19:12:45 +0200

    LongestRep 6kyu

---
+++ src/main/java/exercises/SixKyu/LongestRepetition/RepetitionKata.java
@@ -0,0 +1,29 @@
+package exercises.SixKyu.LongestRepetition;
+
+import java.util.Arrays;
+
+public class RepetitionKata {
+
+    public static void main(String[] args) {
+        Arrays.stream(longestRepetition("abbbccccd")).forEach(System.out::println);
+    }
+
+    public static Object[] longestRepetition(String s) {
+
+        if (s.length() == 0) return new Object[]{"", 0};
+
+        String stringValue = "";
+        int intVlue = 0;
+
+        String[] arr = s.split("(?<=(.))(?!\\1)");
+
+        for (String str : arr) {
+            if (str.length() > intVlue) {
+                stringValue = String.valueOf(str.charAt(0));
+                intVlue = str.length();
+            }
+        }
+
+        return new Object[]{stringValue, intVlue};
+    }
+}
